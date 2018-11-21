f36ce861e0a521a3695c26e2ff6ec9fc4216a123
caoym <2500235257@qq.com>
2018-11-21 20:49:52 +0800

    AAA

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
  */
 const options = {
     RDS_PORT : '6379',                // 端口号
-    RDS_HOST : '127.0.0.1',           // 服务器IP  要连接的A服务器redis
+    RDS_HOST : '172.16.3.21',         // 服务器IP  要连接的A服务器redis
     RDS_PWD  : '',                    // 密码
     RDS_OPTS : {}                     // 设置项
 };
 const url = {
-    host : 'http://59.110.172.228',
+    host : 'http://172.16.3.18',
     users : '/api/v1/users',// 用户列表接口
     document : '/api/v1/share/list'
 };
