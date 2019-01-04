31772fd0a1704723f1e997f39b461beeb9a4ab4e
caoym <2500235257@qq.com>
2019-01-04 17:01:48 +0800

    AA

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
   req.session.user = 'yangjing';
-
   res.send({error: 0, msg: '登录成功'});
 });
 
